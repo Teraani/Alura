8d8eeb6377146c3bc38011ed27c7dddd876a6e1f
Teraani <63324476+Teraani@users.noreply.github.com>
2023-11-03 13:49:56 -0300

    Add files via upload

---
+++ Pictures/truth-falsy.js
@@ -0,0 +1,19 @@
+//let minhaVar;
+//let varNull = null;
+
+//let numero = 3;
+//let texto = "Alura";
+
+//console.log(typeof numero) 
+//console.log(typeof texto)
+
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero) 
+console.log(typeof texto)
+
